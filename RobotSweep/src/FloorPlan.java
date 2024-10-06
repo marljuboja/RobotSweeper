1980655a5a9514c828a1757e7187b5de977d8f58
Marina <mljuboja@luc.edu>
2024-10-06 15:21:15 -0500

    Update #5

--- RobotSweep/src/FloorPlan.java
+++ RobotSweep/src/FloorPlan.java
@@ -15,7 +15,7 @@ public class FloorPlan {
     }
 
     public static String[][] getFloorPlan() {
-        floorPlan[0][0] = "006100";
+        floorPlan[0][0] = "00100100010";
         floorPlan[0][1] = "001000";
         floorPlan[0][2] = "006100";
         floorPlan[0][3] = "001000";
@@ -27,29 +27,27 @@ public class FloorPlan {
         floorPlan[0][9] = "006900";
 
         floorPlan[1][0] = "003000";
-        floorPlan[1][1] = "004041";
-        floorPlan[1][2] = "003031";
+        floorPlan[1][1] = "004049";
+        floorPlan[1][2] = "003039";
         floorPlan[1][3] = "200000";
         floorPlan[1][4] = "210000";
         floorPlan[1][5] = "210000";
         floorPlan[1][6] = "200000";
         floorPlan[1][7] = "200000";
-        floorPlan[1][8] = "004041";
-        floorPlan[1][9] = "007001";
+        floorPlan[1][8] = "004049";
+        floorPlan[1][9] = "007009";
+
         return floorPlan;
     }
-    //floorPlan
+
 
     //floor plan is a 10 by 10 two-dimensional array
     //each number is 4 digits representing four aspects of the tile
     //first digit is tile type; bare, low-pile, or high-pile (0,1,2)
     //second digit is obstacles; nothing, furniture, charging station, stairs  (0,1,2,3)
-    //third is wall; no wall, wall up, wall down, wall left, wall right, up-down, other, left-right (0-7)
-    //fourth is more door options; none, wall up-left, up-right, up-left, down-left, down-right,
-    // down-left-right, up-down-right, up-down-left, up-left-right, (0-9)
-    //fifth is door; no door, door open-up, door open-down, open-left, open-right, door closed-up,
-    // door closed-down, closed-left, closed-right (0-8)
-    //sixth is dirt; no dirt, dirt (0,1)
+    //third, fourth, fifth, sixth; is position of wall N,S,E,W
+    //seventh, eighth, ninth, tenth; is door position, N,S,E,W with 0 -no door, 1- open door, 2- closed door
+    //eleventh is amount of dirt with 0- no dirt; (0-9)
 
     //creating floor plan
 
--- RobotSweep/src/Main.java
+++ RobotSweep/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        FloorPlan.getFloorPlan();
 
         //System.out.println("First point in array" + FloorPlan.floorPlan[0][0]);
         for(int i = 0; i < 10; i++){
