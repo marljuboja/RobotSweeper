42a52b8d103562fa85ea67ed6728da5f21b2dd92
Marina <mljuboja@luc.edu>
2024-10-10 16:05:09 -0500

    Updated code

--- RobotSweep/src/DetectChargeStation.java
+++ RobotSweep/src/DetectChargeStation.java
@@ -1,62 +1,49 @@
 public class DetectChargeStation {
+    public final int maxIndex = 10;
     public DetectChargeStation () {}
 
-    public int[] detectCS (int currentPositionX, int currentPositionY) {
+    public static int[] detectCS (int currentPositionX, int currentPositionY) {
         int[] coor = new int[2];
-        coor[0] = 10;
-        coor[1] = 10;
+        coor[0] = -1;
+        coor[1] = -1;
 
-        if ((FloorPlan.floorPlan[currentPositionX+1][currentPositionY].substring(1,1)) == "2") {
-            coor[0] = currentPositionX+1;
-            coor[1] = currentPositionY;
-        }
-        if ((FloorPlan.floorPlan[currentPositionX][currentPositionY+1].substring(1,1)) == "2") {
-            coor[0] = currentPositionX;
-            coor[1] = currentPositionY+1;
-        }
-        if ((FloorPlan.floorPlan[currentPositionX+1][currentPositionY+1].substring(1,1)) == "2") {
-            coor[0] = currentPositionX+1;
-            coor[1] = currentPositionY+1;
-        }
-        if ((FloorPlan.floorPlan[currentPositionX-1][currentPositionY-1].substring(1,1)) == "2") {
-            coor[0] = currentPositionX-1;
-            coor[1] = currentPositionY-1;
-        }
-        if ((FloorPlan.floorPlan[currentPositionX-1][currentPositionY].substring(1,1)) == "2") {
-            coor[0] = currentPositionX-1;
-            coor[1] = currentPositionY;
-        }
-        if ((FloorPlan.floorPlan[currentPositionX][currentPositionY-1].substring(1,1)) == "2") {
-            coor[0] = currentPositionX;
-            coor[1] = currentPositionY-1;
-        }
-
-        if ((FloorPlan.floorPlan[currentPositionX+2][currentPositionY].substring(1,1)) == "2") {
+        if (((currentPositionX+2) <= 10) && (currentPositionY <= 10) && ((currentPositionX+2) > -1) &&
+                (currentPositionY > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX+2][currentPositionY].substring(0,1)) == "2")) {
             coor[0] = currentPositionX+2;
             coor[1] = currentPositionY;
         }
-        if ((FloorPlan.floorPlan[currentPositionX][currentPositionY+2].substring(1,1)) == "2") {
+        if ((currentPositionX <= 10) && ((currentPositionY+2) <= 10) && (currentPositionX > -1) &&
+                ((currentPositionY+2) > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX][currentPositionY+2].substring(0,1)) == "2")) {
             coor[0] = currentPositionX;
             coor[1] = currentPositionY+2;
         }
-        if ((FloorPlan.floorPlan[currentPositionX+2][currentPositionY+2].substring(1,1)) == "2") {
+        if (((currentPositionX+2) <= 10) && ((currentPositionY+2) <= 10) && ((currentPositionX+2) > -1) &&
+                ((currentPositionY+2) > -1) &&
+                (FloorPlan.floorPlan[currentPositionX + 2][currentPositionY + 2].substring(0, 1).equals("2"))) {
             coor[0] = currentPositionX+2;
             coor[1] = currentPositionY+2;
         }
-        if ((FloorPlan.floorPlan[currentPositionX-2][currentPositionY-2].substring(1,1)) == "2") {
+        if (((currentPositionX-2) <= 10) && ((currentPositionY-2) <= 10) && ((currentPositionX-2) > -1) &&
+                ((currentPositionY-2) > -1) &&
+                (FloorPlan.floorPlan[currentPositionX - 2][currentPositionY - 2].substring(0, 1).equals("2"))) {
             coor[0] = currentPositionX-2;
             coor[1] = currentPositionY-2;
         }
-        if ((FloorPlan.floorPlan[currentPositionX-2][currentPositionY].substring(1,1)) == "2") {
+        if ( ((currentPositionX-2) <= 10) && (currentPositionY <= 10) && ((currentPositionX-2) > -1) &&
+                (currentPositionY > -1) &&
+                (FloorPlan.floorPlan[currentPositionX - 2][currentPositionY].substring(0, 1).equals("2"))) {
             coor[0] = currentPositionX-2;
             coor[1] = currentPositionY;
         }
-        if ((FloorPlan.floorPlan[currentPositionX][currentPositionY-2].substring(1,1)) == "2") {
+        if ((currentPositionX <= 10) && ((currentPositionY-2) <= 10) && (currentPositionX > -1) &&
+                ((currentPositionY-2) > -1) &&
+                (FloorPlan.floorPlan[currentPositionX][currentPositionY - 2].substring(0, 1).equals("2"))) {
             coor[0] = currentPositionX;
             coor[1] = currentPositionY-2;
         }
 
-        //if array has 10's then no charging station was found
         return coor;
 
     }
--- RobotSweep/src/Main.java
+++ RobotSweep/src/Main.java
@@ -2,11 +2,15 @@ public class Main {
     public static void main(String[] args) {
         FloorPlan.getFloorPlan();
 
-        //System.out.println("First point in array" + FloorPlan.floorPlan[0][0]);
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 10; j++){
-                System.out.println(FloorPlan.floorPlan[i][j]);
+                System.out.print(FloorPlan.floorPlan[i][j] + " ");
+                //System.out.print(FloorPlan.floorPlan[i][j].substring(0,1));
             }
+            System.out.println();
         }
+
+        int[] arr = DetectChargeStation.detectCS(1,9);
+        System.out.println(" " + arr[0] + ", " + arr[1]);
     }
 }
