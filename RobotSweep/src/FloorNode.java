24aae6904b7a0d07d5ec66e3d5914963ccd3ac21
STraenkle <ssamowa3l@yahoo.com>
2024-10-13 00:38:18 -0500

    Added FloorNode Object that takes String from entry in 2D floorplan array and converts it into an Object with getters for each of the tile's values. Updated FloorSweeper.java so that it can observe N, S, E, and W to get the String from the floorplan 2D array and creates a FloorNode based off it, which it then stores in its memory.

---
+++ RobotSweep/src/FloorNode.java
@@ -0,0 +1,101 @@
+public class FloorNode {
+    private int dirt;
+    private final int floorType;
+    private final boolean wallNorth, wallSouth, wallEast, wallWest;
+    private int doorNorth, doorSouth, doorEast, doorWest;
+    private int obstacle;
+    private boolean chargingStation;
+
+    public FloorNode(String string){
+        int[] parsed = new int[10];
+        for(int i = 0; i < string.length();i++){
+            parsed[i] = Integer.parseInt(string.substring(i,i+1));
+        }
+        floorType = parsed[0];
+        obstacle = parsed[1];
+        wallNorth = (parsed[2] == 1);
+        wallSouth = (parsed[3] == 1);
+        wallEast = (parsed[4] == 1);
+        wallWest = (parsed[5] == 1);
+        doorNorth = parsed[6];
+        doorSouth = parsed[7];
+        doorEast = parsed[8];
+        doorWest = parsed[9];
+        dirt = parsed[10];
+    }
+
+    //Returns amount of dirt on tile
+    //0 dirt is clean, anything more is dirty
+    public int getDirt(){
+        return dirt;
+    }
+
+    //Reduces dirt amount by 1
+    //0 dirt is clean and thus cannot be cleaned further
+    public int cleanDirt(){
+        if (dirt > 0){
+            dirt--;  
+        }
+        return dirt;
+    }
+
+    //Input cardinal direction (N,S,E,W), returns whether there is wall in that direction
+    public boolean getWall(String string) throws IllegalArgumentException{
+        String temp = string.toLowerCase();
+        if (temp == "north"){
+            return wallNorth;
+        }
+        else if (temp == "south"){
+            return wallSouth;
+        }
+        else if (temp == "east"){
+            return wallEast;
+        }
+        else if (temp == "west"){
+            return wallWest;
+        }
+        throw new IllegalArgumentException ("Invalid direction");
+    }
+
+    //Input cardinal direction(N,S,E,W), returns whether there is door in that direction
+    //0 = No door
+    //1 = Open door (passible)
+    //2 = Closed door (impassible)
+    public int getDoor(String string) throws IllegalArgumentException{
+        String temp = string.toLowerCase();
+        if (temp == "north"){
+            return doorNorth;
+        }
+        else if (temp == "south"){
+            return doorSouth;
+        }
+        else if (temp == "east"){
+            return doorEast;
+        }
+        else if (temp == "west"){
+            return doorWest;
+        }
+        throw new IllegalArgumentException ("Invalid direction");
+    }
+
+    public boolean getChargingStation(){
+        return chargingStation;
+    }
+
+    //Returns floor type
+    //0 = Bare floor
+    //1 = Low-pile carpet
+    //2 = High-pile carpet
+    public int getFloorType(){
+        return floorType;
+    }
+
+    //Returns presence of obstacle on tile
+    //0 = Nothing
+    //1 = Furniture
+    //2 = 
+    //3 = Stairs
+    public int getObstacle(){
+        return obstacle;
+    } 
+}
--- RobotSweep/src/FloorSweeper.java
+++ RobotSweep/src/FloorSweeper.java
@@ -1,26 +1,101 @@
 public class FloorSweeper {
 
     //learnedFloorPlan is the floor plan that the robot learns as it moves
-    String[][] learnedFloorPlan;
+    FloorNode[][] learnedFloorPlan;
     //assignedFloorPlan is the complete floor plan given to the robot to navigate
     //!!!!!!!!!!!!!!!
     //THE ROBOT CANNOT SEARCH THIS FLOOR PLAN EXCEPT FOR ITS IMMEDIATE SURROUNDINGS!!
     //!!!!!!!!!!!!!!!!!
     final String[][] assignedFloorPlan;
     //charge is the robot's remaining battery
-    int charge;
+    private int charge;
     //dirtCapacity is how much dirt the robot is currently holding
-    int dirt;
+    private int dirt;
     //dirtCapacity is the maximum dirt the robot can hold
     final int dirtCapacity;
     //posX and posY are the robots x and y positions, respectively
-    int posX;
-    int posY;
+    private int posX;
+    private int posY;
 
+    //Constructor for robot
+    //Takes floorplan and stores locally, but only accesses entries immediately surrounding its current position
     public FloorSweeper(String[][] floorplan){
-        learnedFloorPlan = new String[10][10];
+        learnedFloorPlan = new FloorNode[10][10];
         assignedFloorPlan = floorplan;
         dirtCapacity = 50;
+        charge = 100;
+    }
+
+    //Returns battery charge level of robot
+    public int getCharge(){
+        return charge;
+    }
+    
+    //Returns amount of dirt stored in robot
+    public int getDirtLevel(){
+        return dirt;
+    }
+
+    //Observes tiles in cardinal directions relative to current position
+    //Useful for quickly mapping to memory after moving
+    public void scanSurroundings(){
+        lookNorth();
+        lookSouth();
+        lookEast();
+        lookWest();
+    }
+
+    //Observes tile north of current position
+    //Returns observations as FloorNode
+    public FloorNode lookNorth(){
+        if (posY <= 0){
+            return null;
+        }
+        else{
+            FloorNode temp = new FloorNode(assignedFloorPlan[posX][posY-1]);
+            learnedFloorPlan[posX][posY-1] = temp;
+            return temp;
+        }
+    }
+
+    //Observes tile east of current position
+    //Returns observations as FloorNode
+    public FloorNode lookEast(){
+        if (posX >= 9){
+            return null;
+        }
+        else{
+            FloorNode temp = new FloorNode(assignedFloorPlan[posX+1][posY]);
+            learnedFloorPlan[posX+1][posY] = temp;
+            return temp;
+        }
+
+    }
+
+    //Observes tile south of current position
+    //Returns observations as FloorNode
+    public FloorNode lookSouth(){
+        if (posY >= 9){
+            return null;
+        }
+        else{
+            FloorNode temp = new FloorNode(assignedFloorPlan[posX][posY+1]);
+            learnedFloorPlan[posX][posY+1] = temp;
+            return temp;
+        }
+    }
+
+    //Observes tile west of current position
+    //Returns observations as FloorNode
+    public FloorNode lookWest(){
+        if (posX <= 0){
+            return null;
+        }
+        else{
+            FloorNode temp = new FloorNode(assignedFloorPlan[posX-1][posY]);
+            learnedFloorPlan[posX-1][posY] = temp;
+            return temp;
+        }
     }
     
 }
