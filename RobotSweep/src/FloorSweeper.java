9671b9f16b7fac659009e5b88d3c58f6235051e1
Marina <mljuboja@luc.edu>
2024-10-18 21:33:13 -0500

    creating two new stories

---
+++ RobotSweep/src/FloorSweeper.java
@@ -0,0 +1,26 @@
+public class FloorSweeper {
+
+    //learnedFloorPlan is the floor plan that the robot learns as it moves
+    String[][] learnedFloorPlan;
+    //assignedFloorPlan is the complete floor plan given to the robot to navigate
+    //!!!!!!!!!!!!!!!
+    //THE ROBOT CANNOT SEARCH THIS FLOOR PLAN EXCEPT FOR ITS IMMEDIATE SURROUNDINGS!!
+    //!!!!!!!!!!!!!!!!!
+    final String[][] assignedFloorPlan;
+    //charge is the robot's remaining battery
+    int charge;
+    //dirtCapacity is how much dirt the robot is currently holding
+    int dirt;
+    //dirtCapacity is the maximum dirt the robot can hold
+    final int dirtCapacity;
+    //posX and posY are the robots x and y positions, respectively
+    int posX;
+    int posY;
+
+    public FloorSweeper(String[][] floorplan){
+        learnedFloorPlan = new String[10][10];
+        assignedFloorPlan = floorplan;
+        dirtCapacity = 50;
+    }
+    
+}
