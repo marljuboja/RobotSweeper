93a52597f7feccfb8284e36210dd45d07df99428
Marina <mljuboja@luc.edu>
2024-10-19 17:58:14 -0500

    Added automatic shut down class

---
+++ RobotSweep/src/ShutDown.java
@@ -0,0 +1,10 @@
+public class ShutDown {
+    String[][] floorMap = FloorPlan.getFloorPlan();
+    public ShutDown () {}
+
+    public void automaticShutDown() {
+        if () {}
+        else if () {}
+    }
+
+}
