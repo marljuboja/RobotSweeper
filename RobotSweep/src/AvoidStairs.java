136ad4397c705b146dd0ec46ff6dcf1824a5e1c8
Marina <mljuboja@luc.edu>
2024-10-18 23:39:45 -0500

    Updated AvoidStairs

--- RobotSweep/src/AvoidStairs.java
+++ RobotSweep/src/AvoidStairs.java
@@ -1,5 +1,64 @@
 public class AvoidStairs {
     public AvoidStairs () {}
 
+    public boolean avoidStairs (int currentPositionX, int currentPositionY) {
+        if (((currentPositionX + 1) < 10) && (currentPositionY < 10) && ((currentPositionX + 1) > -1) &&
+                (currentPositionY > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX + 1][currentPositionY].substring(1, 2).equals("3")))) {
+            if (DetectObstacle.detectObs(currentPositionX + 1, currentPositionY) == [-1,-1]) {
+
+            }
+
+        } else if ((currentPositionX < 10) && ((currentPositionY + 1) < 10) && (currentPositionX > -1) &&
+                ((currentPositionY + 1) > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX][currentPositionY + 1].substring(1, 2).equals("1"))
+                        || (FloorPlan.floorPlan[currentPositionX][currentPositionY + 1].substring(1, 2).equals("2"))
+                        || (FloorPlan.floorPlan[currentPositionX][currentPositionY + 1].substring(1, 2).equals("3")))) {
+
+        } else if (((currentPositionX + 1) < 10) && ((currentPositionY + 1) < 10) && ((currentPositionX + 1) > -1) &&
+                ((currentPositionY + 1) > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX + 1][currentPositionY + 1].substring(1, 2).equals("1"))
+                        || (FloorPlan.floorPlan[currentPositionX + 1][currentPositionY + 1].substring(1, 2).equals("2"))
+                        || (FloorPlan.floorPlan[currentPositionX + 1][currentPositionY + 1].substring(1, 2).equals("3")))) {
+
+        } else if (((currentPositionX - 1) < 10) && ((currentPositionY - 1) < 10) && ((currentPositionX - 1) > -1) &&
+                ((currentPositionY - 1) > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX - 1][currentPositionY - 1].substring(1, 2).equals("1"))
+                        || (FloorPlan.floorPlan[currentPositionX - 1][currentPositionY - 1].substring(1, 2).equals("2"))
+                        || (FloorPlan.floorPlan[currentPositionX - 1][currentPositionY - 1].substring(1, 2).equals("3")))) {
+
+        } else if (((currentPositionX - 1) < 10) && (currentPositionY < 10) && ((currentPositionX - 1) > -1) &&
+                (currentPositionY > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX - 1][currentPositionY].substring(1, 2).equals("1"))
+                        || (FloorPlan.floorPlan[currentPositionX - 1][currentPositionY].substring(1, 2).equals("2"))
+                        || (FloorPlan.floorPlan[currentPositionX - 1][currentPositionY].substring(1, 2).equals("3")))) {
+
+        } else if ((currentPositionX < 10) && ((currentPositionY - 1) < 10) && (currentPositionX > -1) &&
+                ((currentPositionY - 1) > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX][currentPositionY - 1].substring(1, 2).equals("1"))
+                        || (FloorPlan.floorPlan[currentPositionX][currentPositionY - 1].substring(1, 2).equals("2"))
+                        || (FloorPlan.floorPlan[currentPositionX][currentPositionY - 1].substring(1, 2).equals("3")))) {
+
+        } else if ((currentPositionX < 10) && ((currentPositionY) < 10) && (currentPositionX > -1) &&
+                ((currentPositionY) > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX][currentPositionY].substring(1, 2).equals("1"))
+                        || (FloorPlan.floorPlan[currentPositionX][currentPositionY].substring(1, 2).equals("2"))
+                        || (FloorPlan.floorPlan[currentPositionX][currentPositionY].substring(1, 2).equals("3")))) {
+
+        } else if (((currentPositionX - 1) < 10) && ((currentPositionY + 1) < 10) && (currentPositionX > -1) &&
+                ((currentPositionY + 1) > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX - 1][currentPositionY + 1].substring(1, 2).equals("1"))
+                        || (FloorPlan.floorPlan[currentPositionX - 1][currentPositionY + 1].substring(1, 2).equals("2"))
+                        || (FloorPlan.floorPlan[currentPositionX - 1][currentPositionY + 1].substring(1, 2).equals("3")))) {
+
+        } else if (((currentPositionX + 1) < 10) && ((currentPositionY - 1) < 10) && ((currentPositionX + 1) > -1) &&
+                ((currentPositionY - 1) > -1) &&
+                ((FloorPlan.floorPlan[currentPositionX + 1][currentPositionY - 1].substring(1, 2).equals("1"))
+                        || (FloorPlan.floorPlan[currentPositionX + 1][currentPositionY - 1].substring(1, 2).equals("2"))
+                        || (FloorPlan.floorPlan[currentPositionX + 1][currentPositionY - 1].substring(1, 2).equals("3")))) {
+
+        }
+        return true;
+    }
 
 }
