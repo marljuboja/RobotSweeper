20d63d4b4f89ef10e5f2ce656f6cb81609f48e19
Marina <mljuboja@luc.edu>
2024-10-19 11:45:52 -0500

    Updated AvoidStairs to include FloorNode

--- RobotSweep/src/AvoidStairs.java
+++ RobotSweep/src/AvoidStairs.java
@@ -1,11 +1,18 @@
 public class AvoidStairs {
+
+    String[][] floorMap = FloorPlan.getFloorPlan();
     public AvoidStairs () {}
 
     public boolean avoidStairs (int currentPositionX, int currentPositionY) {
+
+        int obs = new FloorNode(floorMap[currentPositionX][currentPositionY]).getObstacle();
+
         if (((currentPositionX + 1) < 10) && (currentPositionY < 10) && ((currentPositionX + 1) > -1) &&
                 (currentPositionY > -1) &&
-                ((FloorPlan.floorPlan[currentPositionX + 1][currentPositionY].substring(1, 2).equals("3")))) {
-            if (DetectObstacle.detectObs(currentPositionX + 1, currentPositionY) == [-1,-1]) {
+                (obs == 3)) {
+            int obs1 = new FloorNode(floorMap[currentPositionX + 1][currentPositionY]).getObstacle();
+
+            if (obs1 == 0) {
                 currentPositionX = currentPositionX + 1;
             }
 
