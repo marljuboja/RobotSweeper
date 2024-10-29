94b2dd4eaeed6a26f1c1cf6fc788c539e1b3e6a7
Marina <mljuboja@luc.edu>
2024-10-29 14:50:29 -0500

    Update Main.java

--- RobotSweep/src/Main.java
+++ RobotSweep/src/Main.java
@@ -3,10 +3,11 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         FloorPlan.getFloorPlan();
+        //
 
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 10; j++){
-                System.out.print("@ " + i + "/" + j + ": " + FloorPlan.floorPlan[i][j] + " ");
+                System.out.print(FloorPlan.floorPlan[i][j] + " ");
             }
             System.out.println();
         }
