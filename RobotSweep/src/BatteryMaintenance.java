292b9ce6a8e8efec87630bd5ac36cea91eb4b836
Marina <mljuboja@luc.edu>
2024-11-01 15:55:18 -0500

    Update BatteryMaintenance.java

--- RobotSweep/src/BatteryMaintenance.java
+++ RobotSweep/src/BatteryMaintenance.java
@@ -7,37 +7,51 @@ public class BatteryMaintenance {
     //
     //After recharging at the charging station, it resumes cleaning.
 
-    public final int maxBattery = 50;
-    public static int currentBatteryLevel = 250;
+    public static final int maxBattery = 50;
+    public static double currentBatteryLevel = 250.0;
     static String[][] floorMap = FloorPlan.getFloorPlan();
+    static int flag = -1;
     public BatteryMaintenance () {}
 
-    public void run() {}
-
-    //MovementPowerCalculator = how much power moving
-    public int[] startBatteryMaintenance (int[] start, int[] dest) {
+    /**
+     * Checks to see how much battery is required to move and if it needs to go to the charging station
+     * @author Marina Ljuboja
+     * @return boolean
+     */
+    public boolean startBatteryMaintenance (int[] start, int[] dest) {
         int typeStart = new FloorNode(floorMap[start[0]][start[1]],start[0],start[1]).getFloorType();
         int typeDest = new FloorNode(floorMap[dest[0]][dest[1]],dest[0],dest[1]).getFloorType();
+        double cleanPower = 0.0;
 
         double movePower = MovementPowerCalculator.calculateMovementPower(typeStart, typeDest);
-        //double cleanPower = MovementPowerCalculator.getPowerRequirement(typeDest);
+
+        if (flag == -1) {
+            cleanPower = MovementPowerCalculator.getPowerRequirement(typeDest);
+        }
 
         double totalPower = movePower + cleanPower;
 
-        if (isEnoughBattery(totalPower)) {
-            return dest;
+        if ((isEnoughBattery(totalPower)) && (flag == -1)) {
+            currentBatteryLevel = currentBatteryLevel - totalPower;
+            return true;
+        }
+        //////// keeps moving charging station without cleaning
+        else if (flag == 0) {
+            currentBatteryLevel = currentBatteryLevel - totalPower;
+            return true;
         }
         else {
-            int[] chargeSt = goesToCS();
-            return chargeSt;
+            flag = 0;
+            ////////// needs to be recharged once at station
+            isAtCS();
+            return false;
         }
 
     }
 
-    //
 
     /**
-     * Looks for charging station within two tiles away from current position
+     * Checks current battery amount to see if there is enough to move to the next tile
      * @author Marina Ljuboja
      * @return boolean
      */
@@ -52,19 +66,14 @@ public class BatteryMaintenance {
         }
     }
 
-    public int[] goesToCS() {
-        int[] locCS = new int[2];
-        
-
-        return locCS;
-    }
-
+    /**
+     * Once at the charging station it updates battery level to full
+     * @author Marina Ljuboja
+     * @return boolean
+     */
     public void isAtCS() {
-        currentBatteryLevel = 250;
+        currentBatteryLevel = 250.0;
     }
 
-    public void isDoneCharging () {
-        //TODO resumes movement and cleaning
-    }
 
 }
