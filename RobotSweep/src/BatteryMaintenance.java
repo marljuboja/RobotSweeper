2c1b9e8a215758d70a87de0e12bccce621d53911
Marina <mljuboja@luc.edu>
2024-10-31 22:14:39 -0500

    Added to BatteryMaintenance

--- RobotSweep/src/BatteryMaintenance.java
+++ RobotSweep/src/BatteryMaintenance.java
@@ -6,7 +6,21 @@ public class BatteryMaintenance {
     //If the power drops below a certain point, the robot stops cleaning and heads back to its charging station to recharge.
     //
     //After recharging at the charging station, it resumes cleaning.
-
+    public final int maxBattery = 50;
+    public static int currentBatteryLevel = 250;
     public BatteryMaintenance () {}
-    
+
+    public boolean isEnoughBattery () {
+        if (currentBatteryLevel >= 50) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void goesToCS() {}
+
+    public void isDoneCharging () {}
+
 }
