6f27ccedb6b6d4729498e8cc27d2646c564f345a
Marina <mljuboja@luc.edu>
2024-11-02 14:47:07 -0500

    Update BatteryMaintenance.java

--- RobotSweep/src/BatteryMaintenance.java
+++ RobotSweep/src/BatteryMaintenance.java
@@ -1,12 +1,5 @@
 public class BatteryMaintenance {
-    //Robot drains the appropriate amount of power when cleaning or moving depending on terrain (bare floor, low-pile, or high-pile)
-    //
-    //Robot makes sure that it stays above a certain threshold of battery power available while cleaning
-    //
-    //If the power drops below a certain point, the robot stops cleaning and heads back to its charging station to recharge.
-    //
-    //After recharging at the charging station, it resumes cleaning.
-
+    
     public static final int maxBattery = 50;
     public static double currentBatteryLevel = 250.0;
     static String[][] floorMap = FloorPlan.getFloorPlan();
